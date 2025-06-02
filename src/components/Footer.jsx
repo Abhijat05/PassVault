830bb5aaf5ac632cfd63cfe4a5dac10dba6905e7
Abhijat05 <abhijatsinha2004@gmail.com>
2025-06-02 18:04:01 +0530

    feat: implement main application structure with Navbar, Manager, and Footer components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            hi this is footer section don't get miss lead by this
+
+
+            <lord-icon
+                src="https://cdn.lordicon.com/iykgtsbt.json"
+                trigger="hover">
+            </lord-icon>
+
+        </footer>
+
+    )
+}
+
+export default Footer
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav className=' bg-slate-800 text-white'>
+            <div className='mycontainer flex items-center justify-between px-4 py-5'>
+
+                <div className="logo font-bold text-2xl">
+
+                    <span className="text-green-700">&lt;</span>
+                    Pass
+                    <span className='text-[#FFD700]'>Vault</span>
+                    <span className="text-green-700">/&gt;</span>
+
+                </div>
+                <ul>
+                    <a href="https://www.wow.xxx/videos/laylani-wylds-wild-ride-with-channing-rodds-huge-cock/">
+                        <lord-icon
+                            src="https://cdn.lordicon.com/cniwvohj.json"
+                            trigger="in"
+                            delay="1500"
+                            state="in-account"
+                            colors="primary:#ffffff">
+                        </lord-icon></a>
+                    <a href="https://www.wow.xxx/videos/laylani-wyld-gets-fucked-by-muscleman-clark-rogers/">
+                        <lord-icon
+                            src="https://cdn.lordicon.com/iykgtsbt.json"
+                            trigger="in"
+                            delay="1500"
+                            state="in-account"
+                            colors="primary:#ffffff">
+                        </lord-icon></a>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
